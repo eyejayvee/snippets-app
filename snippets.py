a7faccb2d1bd22df179933d01efb93baef6d509e
John Vorster <bluglasshead@gmail.com>
2015-01-01 16:27:04 +0000

    Added the main method from Unit 2 Lesson 1.2

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 #Set the output file, and log the level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -9,16 +11,32 @@ def put(name, snippet):
   
   Returns the name of the snippet
   '''
-  logging.error("FIXME: Unimplemented - put({!r}, {!r})")
+  logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
   return name, snippet
 
 def get(name):
   '''
-  Retrieve the snippet with a give name.
+  Retrieve the snippet with a given name.
   
-  If there is no such snippet...
+  If there is no such snippet... # Discuss with Carl (return a blank string?)
   
   Return the snippet.
   '''
   logging.error("FIXME: Unimplemented - get({!r})".format(name))
-  return ""
+  return name #""
+
+# Add a snippet for delete?
+def delete(name):
+  logging.error("FIXME: Unimplemented - delete({!r})".format(name))
+  return name
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
+  
+  
